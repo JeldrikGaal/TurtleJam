8e970fabed2b5df2c4ae255b1949454e24eb3ee7
Loai Qubti <loai17@meet.mit.edu>
2023-05-20 00:40:23 +0200

    Adding UI menus

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,12 +14,18 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI timeTXT;
 
     public GameObject player;
+    private bool paused = false;
 
     // To add
     // Sound logic
     // Level progression
-    // Menus
 
+
+    // Menus
+    [Space(20)]
+    public GameObject pauseMenu;
+    public GameObject gameOverMenu;
+    public GameObject winMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -31,9 +38,33 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape)) Pause();
+
         time+=Time.deltaTime;
 
         scoreTXT.text = "Score: " + score.ToString();
         timeTXT.text = time.ToString("0.00");
     }
+
+    public void Pause() 
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        paused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        paused = false;
+    }
+
+    public void GoToLevel(string levelName) // For Resume, Next Level and Back to Main Menu
+    {
+        if(levelName == "this") SceneManager.LoadScene(SceneManager.loadedSceneCount);
+        SceneManager.LoadScene(levelName);
+    }
+
+
 }
