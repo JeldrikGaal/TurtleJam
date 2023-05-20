3e3b777766fed03a1d93a0c8e28ae3ff5d389b27
Loai Qubti <loai17@meet.mit.edu>
2023-05-20 16:30:29 +0200

    Working on procedural generation

--- Assets/LevelAttributes.cs
+++ Assets/LevelAttributes.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class LevelAttributes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [System.Flags]
+    public enum hasExits
+    {
+        Up = 1 << 0,
+        Down = 1 << 1,
+        Left = 1 << 2,
+        Right = 1 << 3,
+        // Add more options as needed
+    }
+    public hasExits selectedExits;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Check if a specific option is selected
+        if ((selectedExits & hasExits.Up) != 0)
+        {
+            Debug.Log("It has an exit up top!");
+        }
     }
 }
