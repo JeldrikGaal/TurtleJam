5acaa50e82c93a35e33f08e6929aaece98a1fe5a
Loai Qubti <loai17@meet.mit.edu>
2023-05-20 09:39:35 +0200

    Interactables WIP

---
+++ Assets/Interactable.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    public enum type
+    {
+        Slowdown,
+        Speedup
+    }
+
+    public type interactableType;
+
+    public float duration = 3f;
+    public bool activated = false;
+
+    private float time = 0;
+
+    private void Update()
+    {
+        if (activated) 
+        {
+            time += Time.deltaTime;
+            if (time >= duration) 
+            {
+                if (interactableType == type.Slowdown)
+                    DeactivateSlowDown();
+                else if (interactableType == type.Speedup)
+                    DeactivateSpeedUp();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        activated = true;
+        if (interactableType == type.Slowdown)
+            ActivateSlowdown();
+        else ActivateSpeedUp();
+    }
+
+    void ActivateSlowdown() 
+    {
+        Time.timeScale = 0.5f;
+        this.GetComponent<SpriteRenderer>().enabled = false;
+        this.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    void DeactivateSlowDown() 
+    {
+        Time.timeScale = 1f;
+        Destroy(this);
+    }
+
+    void ActivateSpeedUp()
+    {
+        this.GetComponent<SpriteRenderer>().enabled = false;
+        this.GetComponent<BoxCollider2D>().enabled = false;
+
+        GameObject.FindWithTag("Player").GetComponent<PlayerController>().Speed *= 2;
+    }
+
+    void DeactivateSpeedUp()
+    {
+        GameObject.FindWithTag("Player").GetComponent<PlayerController>().Speed /= 2;
+        Destroy(this);
+    }
+
+
+}
