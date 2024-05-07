c3bcbff8b1fbc145addcc9e1f67e4a247db5d06c
JeldrikGaal <jeldrikgaal@gmail.com>
2024-05-07 17:51:42 +0200

    First small pass enemy controller refactoring

--- Assets/_Scripts/World/EnemyController.cs
+++ Assets/_Scripts/World/EnemyController.cs
@@ -78,12 +78,9 @@ public class EnemyController : MonoBehaviour
         // Check if the raycast hit the player
         if (hit.collider != null)
         {
-
             if (hit.collider.gameObject == player.gameObject || hit.transform.CompareTag("Shield"))
             {
                 lookAtPlayer = true;
-                //Debug.Log("Player is in line of sight!");
-                // Do something when the player is in line of sight
             }
             else
             {
@@ -91,145 +88,53 @@ public class EnemyController : MonoBehaviour
             }
         }
 
-        if (enemyMode == EnemyType.Melee)
+        switch (enemyMode)
         {
-            /* // Only moving if line of sight to the player
-            Vector2 dir = player.transform.position - transform.position;
-            dir.Normalize();
-            RaycastHit2D hitout = Physics2D.Raycast(transform.position, dir);
-            if (hit)
-            {
-                //Debug.Log(hit.transform.name);
-                if (hitout.transform.CompareTag("Player"))
-                {
-                   
-                }
-            } */           
-            if (lookAtPlayer)
+            case EnemyType.Melee when lookAtPlayer:
             {
                 Vector3 direction = ( player.position - transform.position).normalized;
-                direction.Normalize();
-                float distance = speed * Time.deltaTime;
                 rb.velocity = direction * speed;
-                
-                /*RaycastHit2D wallhit = Physics2D.Raycast(bulletSpawn.transform.position, transform.forward);
-                Debug.Log(wallhit.transform.name);
-                if (Vector2.Distance(wallhit.point, transform.position) > 2)
-                {
-                    transform.Translate(direction * distance);
-                    Debug.Log(Vector2.Distance(wallhit.point, transform.position));
-                }*/
 
                 if (!GetComponent<AudioSource>().isPlaying) 
                 {
                     GetComponent<AudioSource>().clip = meleeAttackSound;
                     GetComponent<AudioSource>().Play();
                 }
-                //transform.GetChild(0).transform.localRotation = Quaternion.Euler(0f, 0f, transform.GetChild(0).transform.localRotation.z + rotationSpeed * Time.deltaTime);
-                //Debug.Log("Should be rotating");
+                break;
             }
-            else 
+            case EnemyType.Melee:
             {
                 rb.velocity = Vector3.zero; 
-                if (GetComponent<AudioSource>().isPlaying) GetComponent<AudioSource>().Stop(); 
+                if (GetComponent<AudioSource>().isPlaying) GetComponent<AudioSource>().Stop();
+                break;
             }
-        }
-        else if(enemyMode == EnemyType.Patrol) 
-        {
-            // Patrol over patrolPoints
-            if (patrolPoints.Length == 0)
+            case EnemyType.Stationary:
             {
-                Debug.LogWarning("No patrol points assigned.");
-                return;
-            }
+                Vector3 direction = (player.position - transform.position).normalized;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-            Transform currentPatrolPoint = patrolPoints[currentPatrolIndex];
-            Vector3 direction = (currentPatrolPoint.position - transform.position).normalized;
-            float distance = speed * Time.deltaTime;
-
-            transform.Translate(direction * distance);
-
-            if (Vector3.Distance(transform.position, currentPatrolPoint.position) < 0.1f)
-            {
-                if (isPatrollingForward)
+                // Rotate the object around the Z-axis
+                transform.rotation = Quaternion.Euler(0f, 0f, angle);
+                time += Time.deltaTime;
+                if (!shootTimedown)
                 {
-                    currentPatrolIndex++;
-                    if (currentPatrolIndex >= patrolPoints.Length)
-                    {
-                        currentPatrolIndex = patrolPoints.Length - 2;
-                        isPatrollingForward = false;
-                    }
+                    BasicShoot();
                 }
-                else
-                {
-                    currentPatrolIndex--;
-                    if (currentPatrolIndex < 0)
-                    {
-                        currentPatrolIndex = 1;
-                        isPatrollingForward = true;
-                    }
-                }
-            }
-        }
 
-        if (lookAtPlayer)
-        {
-            // Look At Logic
-            {
-            // Get the direction from this object to the target
-            Vector3 direction = player.position - transform.position;
-            direction.Normalize();
-
-            // Calculate the angle in degrees
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-            // Rotate the object around the Z-axis
-            transform.rotation = Quaternion.Euler(0f, 0f, angle);
-            }
-
-            // Shooting Logic
-            time += Time.deltaTime;
-
-            if (enemyMode != EnemyType.Melee)
-            {
-                if (!shootTimedown) { BasicShoot(); }
                 if (time > bulletTimeIntervals)
                 {
                     time = 0;
                     shootTimedown = false;
                 }
+                break;
             }
-        } else
-        {
-            // Look At Logic
-            {
-                // Get the direction from this object to the target
-                Vector3 direction = player.position - transform.position;
-                direction.Normalize();
-
-                // Calculate the angle in degrees
-                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-                // Rotate the object around the Z-axis
-                transform.rotation = Quaternion.Euler(0f, 0f, angle);
-            }
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        // Visualize the line of sight in the Scene view
-        if (player != null)
-        {
-            Gizmos.color = lookAtPlayer ? Color.green : Color.red;
-            Gizmos.DrawLine(transform.position, player.position);
         }
     }
 
     public void BasicShoot() 
     {       
         pa.PlayOneShotSound(0);
-            GameObject newBullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        GameObject newBullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
         shootTimedown = true; // Flag to indicate there should be timedown between each shot.
     }
 
@@ -243,7 +148,7 @@ public class EnemyController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player") 
+        if(collision.gameObject.CompareTag("Player")) 
         {
             collision.gameObject.GetComponent<PlayerController>().Damage(5);
         }
