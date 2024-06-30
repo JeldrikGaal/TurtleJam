1c84c636eca872025236a9364a615e6e2dcde463
JeldrikGaal <jeldrikgaal@gmail.com>
2024-06-30 22:44:31 +0200

    Streak Juice Increase

--- Assets/_Scripts/Juice/StreakIndicator.cs
+++ Assets/_Scripts/Juice/StreakIndicator.cs
@@ -7,22 +7,57 @@ using UnityEngine;
 
 public class StreakIndicator : MonoBehaviour
 {
+    [SerializeField] private int _streakThreshold;
     private TMP_Text _text;
-    private void Awake()
-    {
+    private Sequence _seq;
 
+    [SerializeField] private List<ColorStreakAmountPair> _colorList;
+    
+    [Serializable]
+    struct ColorStreakAmountPair
+    {
+        public Color Color;
+        public int StreakAmount;
+    }
+    
+    private void Awake()
+    { 
         _text = GetComponent<TMP_Text>();
+        _seq = DOTween.Sequence(); 
+    }
+
+    private Color GetColorFromStreak(int streakAmount)
+    {
+        for ( int i = _colorList.Count - 1; i > 0; i--)
+        {
+            if (streakAmount >= _colorList[i].StreakAmount)
+            {
+                return _colorList[i].Color;
+            }
+        }
+
+        return _colorList[0].Color;
     }
 
     public void Activate(int currentStreak)
     {
-        _text.text = "Streak X" + currentStreak;
-        transform.DOScale(Vector3.one * 1.5f, 0.25f).OnComplete(() =>
+        if (_streakThreshold > currentStreak)
         {
-            transform.DOScale(Vector3.one, 0.125f).OnComplete(() =>
-            {
-                Destroy(gameObject, 0.25f);
-            });
+            Destroy(gameObject);
+            return;
+        }
+        
+        _text.text = "Streak X" + currentStreak;
+        _text.color = GetColorFromStreak(currentStreak);
+
+        
+        _seq.Append(transform.DOScale(Vector3.one * 1.5f, 0.25f));
+        _seq.Append(transform.DOScale(Vector3.one, 0.125f));
+        _seq.Append(transform.DOScale(Vector3.zero, 0.125f));
+        _seq.OnComplete(() =>
+        {
+            Destroy(gameObject, 0.25f);
+            _seq.Kill();
         });
     }
 }
